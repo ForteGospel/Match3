7cae4240f26a55af1cfeea6800efe8481bd1e91b
unknown <jonycapo89@hotmail.com>
2025-03-27 13:09:41 +0200

    asd

--- Assets/gridManager.cs
+++ Assets/gridManager.cs
@@ -18,6 +18,7 @@ public class gridManager : MonoBehaviour
     {
         initializeGrid();
         CheckAllForMatch();
+        List<TileController> tilesToDestroy = new List<TileController>();
     }
 
     // Update is called once per frame
@@ -47,40 +48,62 @@ public class gridManager : MonoBehaviour
         {
             for (int j = 0; j < y; j++)
             {
+                Debug.Log(i + " " + j);
                 TileController current = grid[i, j];
+                HashSet<TileController> possibleGems = new HashSet<TileController>();
+                possibleGems.Add(current);
+
+                //left Check
+                if (i != 0 && current.GemType == grid[i - 1, j].GemType)
+                    possibleGems.Add(grid[i - 1, j]);
+                //right check
+                if (i != x - 1 && current.GemType == grid[i + 1, j].GemType)
+                    possibleGems.Add(grid[i + 1, j]);
+                //down Check
+                if (j != 0 && current.GemType == grid[i, j - 1].GemType)
+                    possibleGems.Add(grid[i, j - 1]);
+                //up Check
+                if (j != y - 1 && current.GemType == grid[i, j + 1].GemType)
+                    possibleGems.Add(grid[i, j + 1]);
+
+                if (possibleGems.Count >= 3) 
+                {
+                    foreach (TileController gem in possibleGems)
+                        gem.GetComponent<SpriteRenderer>().color = Color.gray;
+                }
 
                 // Horizontal check
-                if (i < x - 2)
-                {
-                    TileController right1 = grid[i + 1, j];
-                    TileController right2 = grid[i + 2, j];
+                //if (i < x - 2)
+                //{
+                //    TileController right1 = grid[i + 1, j];
+                //    TileController right2 = grid[i + 2, j];
 
-                    if (current != null && right1 != null && right2 != null &&
-                        current.GemType == right1.GemType && current.GemType == right2.GemType)
-                    {
-                        Debug.Log($"Match found at: ({i},{j}) horizontal");
-                        // You could add to a list of matched tiles here
-                        current.GetComponent<SpriteRenderer>().color = Color.gray;
-                        right1.GetComponent<SpriteRenderer>().color = Color.gray;
-                        right2.GetComponent<SpriteRenderer>().color = Color.gray;
-                    }
-                }
+                //    if (current != null && right1 != null && right2 != null &&
+                //        current.GemType == right1.GemType && current.GemType == right2.GemType)
+                //    {
+                //        Debug.Log($"Match found at: ({i},{j}) horizontal");
+                //        // You could add to a list of matched tiles here
+                //        current.GetComponent<SpriteRenderer>().color = Color.gray;
+                //        right1.GetComponent<SpriteRenderer>().color = Color.gray;
+                //        right2.GetComponent<SpriteRenderer>().color = Color.gray;
+                //    }
+                //}
 
-                // Vertical check
-                if (j < y - 2)
-                {
-                    TileController down1 = grid[i, j + 1];
-                    TileController down2 = grid[i, j + 2];
+                //// Vertical check
+                //if (j < y - 2)
+                //{
+                //    TileController down1 = grid[i, j + 1];
+                //    TileController down2 = grid[i, j + 2];
 
-                    if (current != null && down1 != null && down2 != null &&
-                        current.GemType == down1.GemType && current.GemType == down2.GemType)
-                    {
-                        Debug.Log($"Match found at: ({i},{j}) vertical");
-                        current.GetComponent<SpriteRenderer>().color = Color.gray;
-                        down1.GetComponent<SpriteRenderer>().color = Color.gray;
-                        down2.GetComponent<SpriteRenderer>().color = Color.gray;
-                    }
-                }
+                //    if (current != null && down1 != null && down2 != null &&
+                //        current.GemType == down1.GemType && current.GemType == down2.GemType)
+                //    {
+                //        Debug.Log($"Match found at: ({i},{j}) vertical");
+                //        current.GetComponent<SpriteRenderer>().color = Color.gray;
+                //        down1.GetComponent<SpriteRenderer>().color = Color.gray;
+                //        down2.GetComponent<SpriteRenderer>().color = Color.gray;
+                //    }
+                //}
             }
         }
     }
